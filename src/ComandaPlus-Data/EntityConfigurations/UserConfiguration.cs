b507688c9c418529371d46cfdef12ff11e738a94
gc4d <guilhermeacadorin@gmail.com>
2024-09-14 23:45:39 -0300

    FEAT: Up user entity configuration

---
+++ src/ComandaPlus-Data/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,21 @@
+using System;
+using ComandaPlus_Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ComandaPlus_Data.EntityConfigurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Name).HasMaxLength(50).IsRequired();
+        builder.Property(u => u.Email).HasMaxLength(100).IsRequired();
+        builder.Property(u => u.Password).HasMaxLength(50).IsRequired();
+        builder.Property(u => u.Status).HasConversion<int>().IsRequired();
+        builder.Property(u => u.Role).HasConversion<int>().IsRequired();
+        builder.Property(u => u.CreateAt).HasColumnType("datetime").IsRequired();
+        builder.Property(u => u.LastLogin).HasColumnType("datetime");
+    }
+}
