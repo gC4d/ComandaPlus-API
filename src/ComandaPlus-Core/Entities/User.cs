c2b0203df33f321ba7a2ec35ded20a903e5e82b0
gc4d <guilhermeacadorin@gmail.com>
2024-09-17 23:50:49 -0300

    FEAT: Add entities of license and account

--- src/ComandaPlus-Core/Entities/User.cs
+++ src/ComandaPlus-Core/Entities/User.cs
@@ -1,5 +1,7 @@
-using System.ComponentModel.DataAnnotations;
 using ComandaPlus_Core.Entities.Enums;
+using ComandaPlus_Core.Validation.Exceptions;
+using ComandaPlus_Core.Utils.Extensions;
+using System.Text.RegularExpressions;
 
 namespace ComandaPlus_Core.Entities;
 
@@ -11,4 +13,29 @@ public class User : Person
     public UserRole Role { get; private set; }
     public DateTime CreateAt { get; private set; }
     public DateTime LastLogin { get; private set; }
+
+    public User(string name, string email, string password, UserStatus status, DateTime createAt, DateTime lastLogin)
+    {
+
+    }
+    public User(Guid id, string name, string email, string password, UserStatus status, UserRole role, DateTime createdAt, DateTime lastLogin){
+
+    }
+
+    private void Validation(User user){
+        DomainValidationException
+            .When(string.IsNullOrWhiteSpace(user.Name), "Invalid name. Name is required");
+
+        DomainValidationException
+            .When(user.Name.Length < 3, "Invalid name. too short, minimum 3 characters");
+
+        DomainValidationException
+            .When(string.IsNullOrWhiteSpace(user.Email), "Invalid e-mail, E-mail is required"); 
+
+        DomainValidationException
+            .When(!Regex.IsMatch(user.Email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"), "Invalid e-mail");
+
+        DomainValidationException
+            .When(string.IsNullOrWhiteSpace(user.Password), "Invalid password. Password is required");
+    }
 }
