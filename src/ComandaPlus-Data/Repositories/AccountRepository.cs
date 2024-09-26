e8408f070be214f96a1ca0f99a508c2827205d8e
gc4d <guilhermeacadorin@gmail.com>
2024-09-25 23:16:23 -0300

    fix: Account repository was missing contructor

--- src/ComandaPlus-Data/Repositories/AccountRepository.cs
+++ src/ComandaPlus-Data/Repositories/AccountRepository.cs
@@ -1,10 +1,15 @@
 using ComandaPlus_Core.Entities;
 using ComandaPlus_Core.Interfaces.Repositories;
+using ComandaPlus_Data.Context;
 
 namespace ComandaPlus_Data.Repositories;
 
 public class AccountRepository : Repository<Account>, IAccountRepository
 {
+    public AccountRepository(DatabaseContext context) : base(context)
+    {
+    }
+
     public Task<Account> GetLicenseAsync(int? id)
     {
         throw new NotImplementedException();
