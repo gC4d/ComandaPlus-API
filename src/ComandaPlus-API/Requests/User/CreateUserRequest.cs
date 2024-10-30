6243eee53584470372468c279ee16dcd21a9259f
gc4d <guilhermeacadorin@gmail.com>
2024-10-29 23:25:56 -0300

    feat: add create user input model

---
+++ src/ComandaPlus-API/Requests/User/CreateUserRequest.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using ComandaPlus_API.Dtos;
+
+namespace ComandaPlus_API.Requests.User;
+
+public readonly record struct CreateUserRequest
+{
+    [Required]
+    public string Name { get; init;}
+    [Required]
+    [EmailAddress]
+    public string Email { get; init;}
+    [Required]
+    public string Password { get; init;}
+
+    public static implicit operator UserDTO(CreateUserRequest request){
+        return new UserDTO() { 
+            Name = request.Name,
+            Email = request.Email,
+            Password = request.Password,
+            CreateAt = DateTime.Now,
+            Role = Entities.Enums.UserRole.User,
+            Status = Entities.Enums.UserStatus.Active,
+        };
+    }
+}
